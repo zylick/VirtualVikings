8631b63e1da94491edaaad6fc2eec9818694e556
Jesse James Lord <zylick@gmail.com>
2015-05-09 01:13:13 -0700

    Verification of Audio Bug with Android build
    I did some testing with the Scenes and Lightmapping as well as verified
    that the Audio Source was causing the build to crash.

--- Assets/Scripts/3rdParty/Oculus/StartupSample.cs
+++ Assets/Scripts/3rdParty/Oculus/StartupSample.cs
@@ -21,7 +21,7 @@ public class StartupSample : MonoBehaviour
 	public string			sceneToLoad = string.Empty;
 	public GameObject		mSlide0;
 	public GameObject		mSlide1;
-	public AudioSource 		mSlamAudioSource;
+	//public AudioSource 		mSlamAudioSource;
 
 	private float 			mSlideTime = 1.0f;
 
@@ -48,13 +48,13 @@ public class StartupSample : MonoBehaviour
 		//At this point we can time the crossing the Threshold slide down
 		mSlide0.SetActive (true);
 		iTween.MoveTo (mSlide0, iTween.Hash("x", 0.0f, "y", 3.47f, "z", 9.0f, "easeType", "EaseOutQuart", "time", mSlideTime));
-		mSlamAudioSource.Play ();
+		//mSlamAudioSource.Play ();
 
 		yield return new WaitForSeconds (5);
-		mSlamAudioSource.Stop ();
+		//mSlamAudioSource.Stop ();
 
 		mSlide1.SetActive (true);
-		mSlamAudioSource.Play ();
+		//mSlamAudioSource.Play ();
         iTween.MoveTo (mSlide1, iTween.Hash("x", 0.0f, "y", 3.47f, "z", 8.9f, "easeType", "EaseOutQuart", "time", mSlideTime));
 
        	//iTween.MoveTo (mPlayerObject, iTween.Hash("x",hit.point.x ,"y",mPlayerObject.transform.position.y ,"z",hit.point.z , "easeType", "linear", "time", mTimeToWalk / 3));
